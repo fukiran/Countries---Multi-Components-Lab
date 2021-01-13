8571ee355f197f2f805833f4c016f02454bd5061
fukiran <macdorphoto@yahoo.com>
2021-01-13 19:11:11 +0000

    add a click event to the list item which render more detail

---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,18 @@
+<template>
+  <div lang='html'>
+      <h3>Country: {{country.name}}</h3>
+      <p>Capital: {{country.capital}}</p>
+      <p>Population: {{country.population}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "country-detail",
+    props: ['country']
+}
+</script>
+
+<style>
+
+</style>
